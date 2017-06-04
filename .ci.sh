87bca95207b8e55ed1d59d326bffce2867f978c4
Zach Deibert <zachdeibert@gmail.com>
2017-06-03 21:14:23 -0500

    Fixed CI versioning

--- .ci.sh
+++ .ci.sh
@@ -5,5 +5,5 @@ set -e
 msbuild /p:Configuration=Prerelease /p:SolutionDir=$(pwd) *.csproj
 
 # Deploy
-git fetch --unshallow # Fixes version number generation for more than 50 commits
+git fetch --unshallow || true # Fixes version number generation for more than 50 commits
 msbuild /t:CIBuild /p:Configuration=Prerelease /p:SolutionDir=$(pwd) *.csproj
